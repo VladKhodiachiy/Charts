a0babb1b7c22c991fdeed906f4730c12d6f56a4b
Vlad Khodiachiy <vladhod22@gmail.com>
2022-09-13 19:02:42 +0300

    test

--- Source/Charts/Data/Implementations/Standard/ChartDataSet.swift
+++ Source/Charts/Data/Implementations/Standard/ChartDataSet.swift
@@ -528,6 +528,8 @@ extension ChartDataSet: RandomAccessCollection {
 
 // MARK: RangeReplaceableCollection
 extension ChartDataSet: RangeReplaceableCollection {
+    
+    //ADD This
     public func replaceSubrange<C>(_ subrange: Swift.Range<Index>, with newElements: C) where C : Collection, Element == C.Element {
         entries.replaceSubrange(subrange, with: newElements)
         notifyDataSetChanged()
